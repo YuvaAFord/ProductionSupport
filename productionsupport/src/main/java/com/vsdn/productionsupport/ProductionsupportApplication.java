54a9aa3076be8810a90a7291f30cb132fad927e6
Yuvaraj <yuvananand.88@gmail.com>
2020-03-10 20:29:24 +0530

    First Commit

---
+++ productionsupport/src/main/java/com/vsdn/productionsupport/ProductionsupportApplication.java
@@ -0,0 +1,13 @@
+package com.vsdn.productionsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionsupportApplication.class, args);
+	}
+
+}
